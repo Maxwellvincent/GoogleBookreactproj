2656954b0085c107de4ca87886749d343682484e
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-08-07 00:04:40 -0400

    updated filter, need to send to App

--- src/filterForm/filterForm.js
+++ src/filterForm/filterForm.js
@@ -10,23 +10,48 @@ export default class FilterForm extends Component {
         }
     }
 
-    handleChange(){
+    handlePrintChange(printType){
+        this.setState({
+            printType
+        })
+        // console.log(e.target.value);
+        // console.log("this works");
+    }
+
+    handleBookChange(bookType){
+        this.setState({
+            bookType
+        })
+        // console.log(e.target.value);
+        // console.log("this works");
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        // pass to App.js function all data for a new url search
         console.log("this works");
     }
 
     render(){
 
         return(
-            <form className="filter-form">
+            <form className="filter-form" onSubmit={this.handleSubmit}>
                 <label >Print Type: </label>
-                <select id="print-type" value={this.state.printType} onChange={this.handleChange}>
+                <select id="print-type" value={this.state.printType} onChange={(e) => this.handlePrintChange(e.target.value)}>
                     <option value="All">All</option>
+                    <option value="books">Books</option>
+                    <option value="magazines">Magazines</option>
+                    
                 </select>
 
                 <label >Book Type: </label>
-                <select id="book-type" value={this.state.bookType} onChange={this.handleChange}>
+                <select id="book-type" value={this.state.bookType} onChange={(e) => this.handleBookChange(e.target.value)}>
                     <option value="No Filter">No Filter</option>
+                    <option value="ebooks">ebooks</option>
+                    <option value="free-ebooks">free-ebooks</option>
+                    <option value="paid-ebooks">paid-ebooks</option>
                 </select>
+                <button type="submit">Submit</button>
             </form>
         )
     }
